5d1c2ade785a07f476abd1fb689b3298e11cb447
Victor Santos <victorsantos.hf@gmail.com>
2021-04-12 20:58:13 -0300

    adicionando FetchAPI nas requisições AJAX

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -23,4 +23,8 @@ class Negociacao {
     get valor() {
         return this._valor;
     }
+
+    isEquals(outraNegociacao) {
+        return JSON.stringify(this) == JSON.stringify(outraNegociacao);
+    }
 }
--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,18 +1,20 @@
 class HttpService {
 
-    get(url) {
+    _handleErrors(res) {
+        if(!res.ok) throw new Error(res.statusText);
+        return res;
+    }
 
+    get(url) {
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+        
+        /*
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
             xhr.open('GET', url);
 
-            /* Estados requisição AJAX:
-            0: requisição ainda não iniciada
-            1: conexão com o servidor estabelecida
-            2: requisição recebida
-            3: processando requisição
-            4: requisição está concluída e a resposta está pronta
-            */
             xhr.onreadystatechange = () => {
                 if (xhr.readyState == 4) {
                     if (xhr.status == 200) {
@@ -24,13 +26,25 @@ class HttpService {
                     }
                 }
             };
-            /*executa*/
+            //executa
             xhr.send();
         });
+        */
+
     }
 
     post(url, dado) {
 
+        return fetch(url, {
+            headers: { 'Content-type' : 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res => this._handleErrors(res));
+        
+
+
+        /*
         return new Promise((resolve, reject) => {
 
             let xhr = new XMLHttpRequest();
@@ -51,7 +65,8 @@ class HttpService {
                 }
             };
             xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-        });
-
+        }); 
+        */
     }
+    
 }
